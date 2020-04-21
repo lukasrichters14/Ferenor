105a8c39bbe19d43d72d7aa4dfd41db7a0e8f6ea
Lukas <lukasrichters14@gmail.com>
2020-04-21 19:31:40 -0400

    Added master data spreadsheet and a file to store all of the data loading functions.

---
+++ loader.py
@@ -0,0 +1,11 @@
+#
+# This is where all of the functions necessary to return all of the data required by the game are
+# stored. It reads from a master sqlite3 database. This database could be categorized
+# so that functions can only call for specific data and not the entire data set. For example, by
+# regions in the game so you only load in data from one general game location or region.
+#
+
+
+
+
+
